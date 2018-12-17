39a07ac6fed704babe07d105360ffc2177812a51
David Edelstein <yitzedelstein@gmail.com>
2018-12-17 18:25:53 -0500

    first commit random stuff

---
+++ main.js
@@ -0,0 +1,11 @@
+
+//     javarules();
+
+// function javarules() {
+//     <button type="button" onclick="document.getElementById('demo').innerHTML = Date()">
+//         Click me to display Date and Time.</button>;
+// }
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
+  
